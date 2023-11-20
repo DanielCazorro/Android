99a70e2249af6e1384ba504d3555d5212a15b040
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-11-20 22:04:39 +0100

    Practicing basic Kotlin functions

--- Kotlin Language/FirstProyect/src/main/kotlin/Part2.kt
+++ Kotlin Language/FirstProyect/src/main/kotlin/Part2.kt
@@ -1,7 +1,26 @@
 fun main() {
-    println("Testing Part2")
+    println("Starting Part2")
+    println("----------")
+    write()
+    println("----------")
+    writeSomething("Hello World!")
+    println("----------")
+    writeSomethingMore("Hello,", "Daniel")
+    println("----------")
+    writeSomethingMore("Happy Hunting,")
+    println("Ending Part2")
+}
 
+fun write(){
+    println("Entering function: write()")
+}
 
+fun writeSomething(text: String){
+    println("Entering the function: writeSomething()")
+    println(text)
+}
 
-
+fun writeSomethingMore(greeting: String, name: String = "Rathalos"){
+    println("Entering the function writeSomethingMore()")
+    println("$greeting $name")
 }
