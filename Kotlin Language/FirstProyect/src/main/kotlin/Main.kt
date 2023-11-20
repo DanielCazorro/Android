19b0f2f4b6f79012c366818751b417de54e1e570
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-11-20 20:58:25 +0100

    Var opperations

--- Kotlin Language/FirstProyect/src/main/kotlin/Main.kt
+++ Kotlin Language/FirstProyect/src/main/kotlin/Main.kt
@@ -38,6 +38,9 @@ fun main() {
     val division = integer1.toDouble()/3
     println("Value of division = $division  (Should be 2.333333)")
 
+    integer1 = 7
+    val module = integer1.rem(3)
+    println("Value of module = $module (Should be 1)")
 
     var double = 1.0
     println(double)
@@ -51,9 +54,14 @@ fun main() {
     println(letter)
 
     var text = "Hello world!"
-    println(text)
+    println("${text[0]} ${text[1]}")
 
     val text2: String
     text2 = "How are you?"
     println(text2)
+
+    var isFloat: Boolean = float is Float
+    println("The float is Float? = $isFloat")
+
+    
 }
