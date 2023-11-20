9c067679a03d9ddd62fc897727c40fbce123ad73
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-11-20 22:15:30 +0100

    More functions

--- Kotlin Language/FirstProyect/src/main/kotlin/Part2.kt
+++ Kotlin Language/FirstProyect/src/main/kotlin/Part2.kt
@@ -1,13 +1,31 @@
+import kotlin.random.Random
+
 fun main() {
     println("Starting Part2")
+
     println("----------")
     write()
+
     println("----------")
     writeSomething("Hello World!")
+
     println("----------")
     writeSomethingMore("Hello,", "Daniel")
+
     println("----------")
     writeSomethingMore("Happy Hunting,")
+
+
+    println("----------")
+    println("getRandomNumber: ${getRandomNumber()}")
+
+    println("----------")
+    println("getRandomNumberEasy: ${getRandomNumberEasy()}")
+
+    println("----------")
+    println(writeGreetingsWithNull(null, null))
+
+    println("----------")
     println("Ending Part2")
 }
 
@@ -23,4 +41,15 @@ fun writeSomething(text: String){
 fun writeSomethingMore(greeting: String, name: String = "Rathalos"){
     println("Entering the function writeSomethingMore()")
     println("$greeting $name")
+}
+
+fun getRandomNumber(): Int {
+    return Random.nextInt()
+}
+
+fun getRandomNumberEasy() = Random.nextInt()
+
+fun writeGreetingsWithNull(greeting: String?, name: String?) {
+    println("Starting function writeGreetingsWithNull")
+    println("${greeting ?: "Greeting was null"}${name ?: "Name was null"}")
 }
