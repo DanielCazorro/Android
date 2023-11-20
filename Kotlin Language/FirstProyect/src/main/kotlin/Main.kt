84f585fdf306e5354cc3ff088aec5695bc8a66be
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-11-20 20:19:47 +0100

    Basic Bool Types

--- Kotlin Language/FirstProyect/src/main/kotlin/Main.kt
+++ Kotlin Language/FirstProyect/src/main/kotlin/Main.kt
@@ -1,3 +1,14 @@
 fun main() {
-    var a = "bb"
+
+    var booleanTrue = true
+    val booleanFalse = false
+
+    // Si yo cambio el valor de booleanTrue, va a funcionar
+    booleanTrue = false
+    println(booleanTrue)
+
+    // Si yo cambio el valor de booleanFalse, no va a funcionar
+    // booleanFalse = true
+
+
 }
