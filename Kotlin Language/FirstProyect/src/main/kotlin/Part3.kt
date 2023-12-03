89029ec047d112bc7b3a60865a836bd7613f63ad
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-12-03 09:47:16 +0100

    When logic and theory

--- Kotlin Language/FirstProyect/src/main/kotlin/Part3.kt
+++ Kotlin Language/FirstProyect/src/main/kotlin/Part3.kt
@@ -1,3 +1,5 @@
+import kotlin.random.Random
+
 fun main() {
     println("-----ifClassic()-----")
     println("Should be: are not the same")
@@ -9,6 +11,16 @@ fun main() {
     ifClassicEasy(1,2)
     println("Should be: are the same")
     ifClassicEasy(1,1)
+    println("-----whenConParametro ------")
+    whenWithParam(Random.nextInt(0,5))
+    println("-----whenSinParametro ------")
+    whenWithoutParam(Random.nextInt(0,5))
+
+    val int = 1
+    val float = 1.0f
+    println("-----Comparando clases When-----")
+    whenComparingClasses(int)
+    whenComparingClasses(float)
 }
 
 fun ifClassic(number1: Int, number2: Int) {
@@ -23,3 +35,51 @@ fun ifClassicEasy(number1: Int, number2: Int) =
         println("Are the same")
     else
         println("Are not the same")
+
+fun whenWithParam(num: Int) {
+    println(num)
+    when (num) {
+        1 -> {
+            println("Soy un uno")
+            println("Es mi número favorito")
+        }
+        2 -> println("Soy un dos")
+        3 -> println("Soy un tres")
+        4,5 -> println("Soy un cuatro o un cinco")
+
+        else -> println("Soy un número distinto a uno, dos y tres")
+    }
+}
+
+fun whenWithoutParam(num: Int) {
+    println(num)
+    when {
+        num == 1 -> {
+            println("Soy un uno")
+            println("Es mi número favorito")
+        }
+        (num > 1) && (num < 300) -> {
+            println("Soy un número mayor que uno")
+        }
+        else -> println("Soy otro número")
+    }
+}
+
+fun whenComparingClasses(num: Number) {
+    println(num)
+    when (num) {
+        is Int -> {
+            println("Es un entero")
+            // Ahora puedo acceder a todos los métodos de los enteros
+            num.toFloat()
+        }
+        is Float -> {
+            println("Es un float")
+            // Ahora puedo acceder a todos los métodos de los enteros
+            num.toInt()
+        }
+        else -> {
+            println("Es otra cosa")
+        }
+    }
+}
