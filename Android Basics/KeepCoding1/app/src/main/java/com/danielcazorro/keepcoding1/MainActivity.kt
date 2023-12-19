54fafd66f40bc0ff396fe17d7fde8c22fa324498
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-12-19 16:12:03 +0100

    Layout basic training

--- Android Basics/KeepCoding1/app/src/main/java/com/danielcazorro/keepcoding1/MainActivity.kt
+++ Android Basics/KeepCoding1/app/src/main/java/com/danielcazorro/keepcoding1/MainActivity.kt
@@ -7,7 +7,7 @@ import android.util.Log
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_linear_layout)
         println("Soy el println")
         Log.d("TAG", "Soy el log nivel d")
         Log.i("TAG", "Soy el log nivel i")
