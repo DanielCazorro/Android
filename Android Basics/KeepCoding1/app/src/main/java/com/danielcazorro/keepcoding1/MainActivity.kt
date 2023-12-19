6d704e5120ac2594a12d2308d44389496ea6b8b6
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-12-19 10:37:01 +0100

    Logs

--- Android Basics/KeepCoding1/app/src/main/java/com/danielcazorro/keepcoding1/MainActivity.kt
+++ Android Basics/KeepCoding1/app/src/main/java/com/danielcazorro/keepcoding1/MainActivity.kt
@@ -2,10 +2,18 @@ package com.danielcazorro.keepcoding1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        println("Soy el println")
+        Log.d("TAG", "Soy el log nivel d")
+        Log.i("TAG", "Soy el log nivel i")
+        Log.w("TAG", "Soy el log nivel w")
+        Log.e("TAG", "Soy el log nivel e")
+        Log.wtf("TAG", "Soy el log nivel wtf")
+
     }
 }
